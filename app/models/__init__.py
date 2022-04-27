357b82ce95431297b51ed3e514621cabf272d671
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2022-04-27 17:34:16 -0300

    feat: create recipe_ingredient table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 from .user_model import UserModel
 from .recipe_model import RecipeModel
-from .quantity_model import QuantityModel
 from .ingredient_model import IngredientModel
+from .recipe_ingredient_table import RecipeIngredientModel
