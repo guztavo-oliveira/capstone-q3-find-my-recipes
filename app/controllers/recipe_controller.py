5535062ad0ebf48d1ede7cfe4e6886baebabe32e
Lucas <lcssreiser@gmail.com>
2022-04-27 16:37:42 -0300

    feat: recipe routes completed

---
+++ app/controllers/recipe_controller.py
@@ -0,0 +1,14 @@
+def get_recipes():
+    ...
+
+
+def post_a_recipe():
+    ...
+
+
+def update_a_recipe(recipe_id):
+    ...
+
+
+def delete_a_recipe(recipe_id):
+    ...
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from .user_bp import bp as user_bp
 from .feed_bp import bp as feed_bp
+from .recipe_bp import bp as recipe_bp
+
 
 def init_app(app: Flask):
     app.register_blueprint(user_bp)
     app.register_blueprint(feed_bp)
+    app.register_blueprint(recipe_bp)
