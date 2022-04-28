435d8b657e61ebb211e62dd8f1a05b78f69fa3ba
murillotlopes <murillo.tlopes@hotmail.com>
2022-04-28 14:33:07 -0300

    fix(ORM-FeedModel): inclusion of FeedModel in ORM trace list

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .user_model import UserModel
 from .recipe_model import RecipeModel
 from .ingredient_model import IngredientModel
 from .recipe_ingredient_table import RecipeIngredientModel
+from .feed_model import FeedModel
