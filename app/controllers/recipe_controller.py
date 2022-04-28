dfbd7930c3ca09853eb8d355def661934f0ae3d3
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2022-04-28 14:21:54 -0300

    feat: create get_recipes function

--- app/controllers/recipe_controller.py
+++ app/controllers/recipe_controller.py
@@ -1,4 +1,16 @@
+from http import HTTPStatus
+from flask import jsonify
+from app.configs.database import db
+from app.models.recipe_model import RecipeModel
+
 def get_recipes():
+    base_query = db.session.query(RecipeModel)
+    all_recipes = base_query.order_by(RecipeModel.recipe_id).all()
+
+    return jsonify(all_recipes), HTTPStatus.OK
+
+
+def get_a_recipe_by_id():
     ...
 
 
