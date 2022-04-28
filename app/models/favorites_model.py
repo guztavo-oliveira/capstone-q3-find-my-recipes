b28c0c439103d5b07d5b36520ad879eb0ba401f5
Luan <luandiniz91@hotmail.com>
2022-04-28 10:32:18 -0300

    feat: creat favorites table

--- app/models/favorites_model.py
+++ app/models/favorites_model.py
@@ -1,20 +1,18 @@
 from app.configs.database import db
 from marshmallow import Schema, fields
 from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy.dialects.postgresql import UUID
 
 
 class FavoritesSchema(Schema):
     favorite_id = fields.Int()
     user_id = fields.Int()
-    # reciped_id = fields.Int()
+    reciped_id = fields.Int()
 
 class FavoritesModel(db.Model):
     __tablename__ = "favorites"
 
-    favorite_id: int 
-    user_id: int  
-    # reciped_id: int
 
     favorite_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("user.user_id"), nullable=False)
-    # reciped_id: Column(Integer, ForeignKey("reciped.recipe_id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.user_id"), nullable=False)
+    reciped_id: Column(Integer, ForeignKey("reciped.recipe_id"), nullable=False)
