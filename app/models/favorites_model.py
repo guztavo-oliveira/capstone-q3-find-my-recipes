d45589e47fd0f1253f8ff3768c1168eb890d7b95
Luan <luandiniz91@hotmail.com>
2022-04-28 16:30:57 -0300

    fix: change in the foreignkey of the recipe_id

--- app/models/favorites_model.py
+++ app/models/favorites_model.py
@@ -7,12 +7,11 @@ from sqlalchemy.dialects.postgresql import UUID
 class FavoritesSchema(Schema):
     favorite_id = fields.Int()
     user_id = fields.Int()
-    reciped_id = fields.Int()
+    recipes_id = fields.Int()
 
 class FavoritesModel(db.Model):
     __tablename__ = "favorites"
 
-
     favorite_id = Column(Integer, primary_key=True)
     user_id = Column(UUID(as_uuid=True), ForeignKey("user.user_id"), nullable=False)
-    reciped_id: Column(Integer, ForeignKey("reciped.recipe_id"), nullable=False)
+    reciped_id = Column(Integer, ForeignKey("recipes.recipe_id"), nullable=False)
