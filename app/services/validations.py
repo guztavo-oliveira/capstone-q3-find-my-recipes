0322ab44473ad0783e8e1bf3c12356efe9607c43
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2022-05-05 16:38:27 -0300

    refactor: merge keys and values validations

--- app/services/validations.py
+++ app/services/validations.py
@@ -1,4 +1,4 @@
-from app.exc.user_exc import InvalidValuesError
+from app.exc.user_exc import InvalidValuesError, InsufficienDataKeyError, InvalidKeysError
 
 def serialize_data(data: dict):
     for key, value in data.items():
@@ -8,11 +8,30 @@ def serialize_data(data: dict):
             data[key] = value.lower()
 
 
-def validate_value_type(data: dict):
+def validate_keys_and_value_type(data: dict, valid_keys, update=False):
     for key, value in data.items():
-        if key == "serves":
-            isinstance(value, int)
-        else:
-            isinstance(value, str)
+        if key not in valid_keys:
+            invalid_keys.append(key)
 
-        # fazer raise do invalid values
+        if key == "serves":
+            if not isinstance(value, int):
+                raise InvalidValuesError(key, value)
+
+        else:
+            if not isinstance(value, str):
+                raise InvalidValuesError(key, value)
+
+    invalid_keys = []
+
+    if update:
+        for key, value in data.items():
+            if key not in tuple(valid_keys):
+                raise InvalidKeysError(valid_keys, key)
+
+    else:
+        if len(data) < len(valid_keys):
+            raise InsufficienDataKeyError(valid_keys)
+        
+
+    if invalid_keys:
+        raise InvalidKeysError(valid_keys, invalid_keys)
